44a86d554b3d4f321e768928546130956afeef69
feakos <fekete@feketeakos.com>
2017-02-05 17:29:00 +0100

    Fehér színű lett az ablak és némi hibakezelést is kapott itt-ott

--- main.c
+++ main.c
@@ -12,32 +12,55 @@ int main( int argc, char* args[] ){
 	bool quit = false;
 	SDL_Event e;
 
-	//SDL inicializálása
-	SDL_Init( SDL_INIT_VIDEO );
-	
-	//SDL ablak létrehozása
-	SDL_Window *window = SDL_CreateWindow( "Hello SDL2", 
-		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
-		WINDOW_WIDTH, WINDOW_HEIGTH, SDL_WINDOW_OPENGL );
-/*
-	while( !quit ){
-		while( SDL_PollEvent( &e ) != 0 ){
-			if( e.type == SDL_QUIT ) { 
-				quit = true;
-			}
-		}
-		SDL_Delay(50);
-	}
-	*/
+	//Window
+ 	SDL_Window* window = NULL;
 
-	while( !quit ){
-		if(SDL_WaitEvent(&e)) {
-			if( e.type == SDL_QUIT ) { 
-				quit = true;
+	//Surface
+ 	SDL_Surface* screenSurface = NULL;
+	
+	//SDL inicializálása
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 ){
+		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+	}
+	else{
+		//SDL ablak létrehozása
+		SDL_Window *window = SDL_CreateWindow( "Hello SDL2", 
+			SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
+			WINDOW_WIDTH, WINDOW_HEIGTH, SDL_WINDOW_OPENGL );
+	 	if( window == NULL ){
+	 		printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+	 	}
+	 	else{
+		 	//Get window surface 
+		 	screenSurface = SDL_GetWindowSurface( window );
+		 	//Fill the surface white
+		 	SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+		 	//Update the surface 
+		 	SDL_UpdateWindowSurface( window );
+		 	//Wait two seconds
+		 	SDL_Delay( 2000 );
+	 	}
+
+	/*
+		while( !quit ){
+			while( SDL_PollEvent( &e ) != 0 ){
+				if( e.type == SDL_QUIT ) { 
+					quit = true;
+				}
 			}
-		}
-		else{
 			SDL_Delay(50);
+		}
+		*/
+
+		while( !quit ){
+			if(SDL_WaitEvent(&e)) {
+				if( e.type == SDL_QUIT ) { 
+					quit = true;
+				}
+			}
+			else{
+				SDL_Delay(50);
+			}
 		}
 	}
 	
