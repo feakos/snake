830b0bf04f2c293e7e873bbb609d8ce7a5d04c4f
feakos <fekete@feketeakos.com>
2017-02-05 16:49:34 +0100

    Ha becsukom az ablakot, akkor lép ki

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <SDL2/SDL.h>
 
 const int WINDOW_WIDTH = 800;
@@ -7,18 +8,26 @@ const int WINDOW_HEIGTH = 600;
 
 int main( int argc, char* args[] ){
 	
+	bool quit = false;
+	SDL_Event e;
+
 	//SDL inicializálása
-	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Init( SDL_INIT_VIDEO );
 	
 	//SDL ablak létrehozása
-	SDL_Window *window = SDL_CreateWindow("Hello SDL2", 
+	SDL_Window *window = SDL_CreateWindow( "Hello SDL2", 
 		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
-		WINDOW_WIDTH, WINDOW_HEIGTH, SDL_WINDOW_OPENGL);
+		WINDOW_WIDTH, WINDOW_HEIGTH, SDL_WINDOW_OPENGL );
 
+	while( !quit ){
+		while( SDL_PollEvent( &e ) != 0 ){
+			if( e.type == SDL_QUIT ) { 
+				quit = true;
+			}
+		}
+	}
 
-	SDL_Delay(2000);
-	SDL_DestroyWindow(window);
-
+	SDL_DestroyWindow( window );
 	SDL_Quit();
 	return 0;
 }
