41be24e9de803b9b9ced62de001aaf7983d4d4cf
feakos <fekete@feketeakos.com>
2017-02-05 22:59:12 +0100

    wasd gombokra már kiírja konzolban, hogy épp melyiket nyomtad meg. SDL_KEYDOWN ; e.key.keysym.sym ; SDLK_w ...

--- main.c
+++ main.c
@@ -18,12 +18,17 @@ int main( int argc, char* args[] ){
 	//Surface
  	SDL_Surface* screenSurface = NULL;
 	
+	//Render
+	//SDL_Renderer *render;
+
 	//SDL inicializálása
 	if( SDL_Init( SDL_INIT_VIDEO ) < 0 ){
 		fprintf( stderr, "SDL-t nem sikerült inicializálni! SDL_Error: %s\n", SDL_GetError() );
 	}
 	else{
 		//SDL ablak létrehozása
+
+
 		SDL_Window *window = SDL_CreateWindow( "Hello SDL2", 
 			SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
 			WINDOW_WIDTH, WINDOW_HEIGTH, SDL_WINDOW_OPENGL );
@@ -36,7 +41,7 @@ int main( int argc, char* args[] ){
 		 	
 		 	//Kitölti az ablakot fehérrel
 		 	SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-		 	
+
 		 	//Update the surface 
 		 	SDL_UpdateWindowSurface( window );
 	 	}
@@ -53,13 +58,29 @@ int main( int argc, char* args[] ){
 		*/
 
 		while( !quit ){
-			if(SDL_WaitEvent(&e)) {
+			if( SDL_WaitEvent( &e ) ) {
 				if( e.type == SDL_QUIT ) { 
 					quit = true;
 				}
+				else if( e.type == SDL_KEYDOWN ){
+					switch( e.key.keysym.sym ){
+						case SDLK_w:
+							printf( "W key\n" );
+							break;
+						case SDLK_a:
+							printf( "A key\n" );
+							break;
+						case SDLK_s:
+							printf( "S key\n" );
+							break;
+						case SDLK_d:
+							printf( "D key\n" );
+							break;
+					}
+				}
 			}
 			else{
-				SDL_Delay(50);
+				SDL_Delay( 50 );
 			}
 		}
 	}
