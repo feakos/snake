7175ed74cebf7b798fece84ed6e3f895b0f1e8e4
feakos <fekete@feketeakos.com>
2017-02-06 13:42:53 +0100

    Átírtam az irányítást egy működő snake program alapján. Ha megnyomom az adott irányt, akkor elvileg addig marad nyomva, amíg egy másik irányt meg nem nyomok. Ja és mindemellett figyeli, hogy ellentétes irányt ne tudjak nyomni egy adott irány után!

--- main.c
+++ main.c
@@ -7,10 +7,10 @@
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGTH = 800;
 
-int main( int argc, char* args[] ){
+int main ( int argc, char* args[] ){
 	
 	bool quit = true;
-	SDL_Event e;
+	SDL_Event event;
 
 	//Window
  	SDL_Window* window = NULL;
@@ -19,39 +19,122 @@ int main( int argc, char* args[] ){
  	SDL_Surface* screenSurface = NULL;
 	
 	//Render
-	//SDL_Renderer *render;
+	SDL_Renderer *render;
+
+    //Booleans to control the movement
+	bool UP = false;
+	bool W = false;
+	bool DOWN = false;
+	bool S = false;
+	bool RIGHT = false;
+	bool D = false;
+	bool LEFT = false;
+	bool A = false;
 
 	//SDL inicializálása
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 ){
-		fprintf( stderr, "SDL-t nem sikerült inicializálni! SDL_Error: %s\n", SDL_GetError() );
+	if( SDL_Init ( SDL_INIT_VIDEO ) < 0 ){
+		fprintf ( stderr, "SDL-t nem sikerült inicializálni! SDL_Error: %s\n", SDL_GetError() );
 	}
 	else{
 
 		//SDL ablak létrehozása
-		SDL_Window *window = SDL_CreateWindow( "Hello SDL2", 
+		SDL_Window *window = SDL_CreateWindow ( "Hello SDL2", 
 			SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
 			WINDOW_WIDTH, WINDOW_HEIGTH, SDL_WINDOW_OPENGL );
-	 	if( window == NULL ){
-	 		fprintf( stderr, "Az ablakot nem sikerült létrehozni! SDL_Error: %s\n", SDL_GetError() );
+	 	if ( window == NULL ) {
+	 		fprintf ( stderr, "Az ablakot nem sikerült létrehozni! SDL_Error: %s\n", SDL_GetError() );
 	 	}
-	 	else{
-		 	//Window surface
-		 	screenSurface = SDL_GetWindowSurface( window );
-		 	
-		 	//Kitölti az ablakot fehérrel
-		 	SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+	 	
+	 	render = SDL_CreateRenderer (window, -1, SDL_RENDERER_ACCELERATED);
 
-		 	//Update the surface 
-		 	SDL_UpdateWindowSurface( window );
-	 	}
 
-		while( quit ){
-			if( SDL_PollEvent( &e ) != 0 ) {
-				if( e.type == SDL_QUIT ) { 
+	 	//Window surface
+		screenSurface = SDL_GetWindowSurface ( window );
+		//Kitölti az ablakot fehérrel
+		SDL_FillRect ( screenSurface, NULL, SDL_MapRGB ( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+	 	//Update the surface 
+	 	SDL_UpdateWindowSurface ( window );
+	 	
+		while( quit ) {
+			if( SDL_PollEvent( &event ) != 0 ) {
+				if( event.type == SDL_QUIT ) { 
 					quit = false;
 				}
-				else if( e.type == SDL_KEYDOWN ){
-					switch( e.key.keysym.sym ){
+				if (event.type == SDL_KEYDOWN) {
+					if( event.key.keysym.sym == SDLK_q ) {
+						printf( "Quit!\n" );
+						exit(1);
+					}
+					//Then check for the key being pressed and change direction accordingly
+					if ( ( DOWN == false && event.key.keysym.scancode == SDL_SCANCODE_UP ) || 
+						( S == false && event.key.keysym.scancode == SDL_SCANCODE_W ) ) {
+						UP = true;
+						W = true;
+						
+						LEFT = false;
+						A = false;
+						
+						RIGHT = false;
+						D = false;
+						
+						DOWN = false;
+						S = false;
+						
+						printf( "UP & W\n" );
+					}
+					else if ( ( RIGHT == false && event.key.keysym.scancode == SDL_SCANCODE_LEFT ) || 
+						( D == false && event.key.keysym.scancode == SDL_SCANCODE_A ) ) {
+						UP = false;
+						W = false;
+						
+						LEFT = true;
+						A = true;
+						
+						RIGHT = false;
+						D = false;
+						
+						DOWN = false;
+						S = false;
+						
+						printf( "LEFT & A\n" );						
+					}
+					else if ( ( UP == false && event.key.keysym.scancode == SDL_SCANCODE_DOWN ) ||
+						( W == false && event.key.keysym.scancode == SDL_SCANCODE_S ) ) {
+						UP = false;
+						W = false;
+						
+						LEFT = false;
+						A = false;
+						
+						RIGHT = false;
+						D = false;
+						
+						DOWN = true;
+						S = true;
+						
+						printf( "DOWN & S\n" );						
+					}
+					else if ( ( LEFT == false && event.key.keysym.scancode == SDL_SCANCODE_RIGHT ) || 
+						( A == false && event.key.keysym.scancode == SDL_SCANCODE_D ) ) {
+						UP = false;
+						W = false;
+						
+						LEFT = false;
+						A = false;
+						
+						RIGHT = true;
+						D = true;
+						
+						DOWN = false;
+						S = false;
+						
+						printf( "RIGHT & D\n" );						
+					}				
+				}
+
+/*
+				else if( event.type == SDL_KEYDOWN ){
+					switch( event.key.keysym.sym ){
 						case SDLK_w:
 						case SDLK_UP:
 							printf( "W & UP\n" );
@@ -73,14 +156,15 @@ int main( int argc, char* args[] ){
 							exit(1);
 							break;
 					}
-				}
+				}*/
 			}
 			else{
 				SDL_Delay( 50 );
 			}
 		}
 	}
-	
+
+    SDL_RenderPresent( render );
 	SDL_DestroyWindow( window );
 	SDL_Quit();
 
